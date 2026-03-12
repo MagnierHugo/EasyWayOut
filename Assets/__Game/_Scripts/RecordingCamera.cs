ad65ed17bfc1331fba4b4def3a891b9432a4c3b7
Raytroll <80167098+Raytroll@users.noreply.github.com>
2026-03-13 00:26:52 +0100

    Fixed the Recording Camera

--- Assets/__Game/_Scripts/RecordingCamera.cs
+++ Assets/__Game/_Scripts/RecordingCamera.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class RecordingCamera : MonoBehaviour {
     [SerializeField] private float ViewRange = 45.0f;
-    [SerializeField] private float RotateSpeed = 100.5f;
+    [SerializeField] private float RotateSpeed = 0.5f;
     [SerializeField] private float YOffset = 0.0f;
     private float CurrentTime = 0.0f;
 
     void Update() {
         CurrentTime += Time.deltaTime;
-        transform.rotation = Quaternion.Euler(0f, (Mathf.Cos(CurrentTime) * ViewRange) + YOffset, 0f);
+        transform.rotation = Quaternion.Euler(0f, (Mathf.Cos(CurrentTime * RotateSpeed) * ViewRange) + YOffset, 0f);
     }
 }
