98c12953b702410bff5aad2327ffb6b5e92c5edd
MagnierHugo <magnierhugo@proton.me>
2026-03-13 12:02:47 +0100

    lot

--- Assets/__Game/Guns/Revolver/Revolver.cs
+++ Assets/__Game/Guns/Revolver/Revolver.cs
@@ -1,8 +1,19 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Revolver : Gun, IHaveSpecial
 {
     private Mag mag = new Mag();
+
+    [Header("Components")]
+    [SerializeField] private ParticleSystem muzzleFlash;
+    [SerializeField] private Animator animator;
+
+    [Header("Audio")]
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip revolverCockingAudio;
+    [SerializeField] private AudioClip revolverFireAudio;
+    [SerializeField] private AudioClip revolverEmptyAudio;
 
     private void Awake()
     {
@@ -12,19 +23,33 @@ public class Revolver : Gun, IHaveSpecial
 
     public override void Shoot(IShootable target)
     {
-        bool hasBullet = mag.GetBullet();
+        animator.SetTrigger("Cock Hammer");
+        audioSource.clip = revolverCockingAudio;
+        audioSource.Play();
+        var cockBehaviour = animator.GetBehaviour<Cock_Behaviour>();
 
-        // 3. Resolve the shot
-        if (hasBullet)
+        // Would have used lambda but it can't ref itself
+        void OnCockingAnimationEnd()
         {
+            cockBehaviour.onCockAnimationEnd -= OnCockingAnimationEnd;
+            bool bullet = mag.GetBullet();
+            if (!bullet)
+            {
+                Debug.Log("Click.");
+                target.EmptyShot();
+                audioSource.clip = revolverEmptyAudio;
+                audioSource.Play();
+            }
+
             Debug.Log("BANG!");
             target.GetShot();
+            muzzleFlash.Play();
+            audioSource.clip = revolverFireAudio;
+            audioSource.Play();
+
         }
-        else
-        {
-            Debug.Log("Click.");
-            target.EmptyShot();
-        }
+
+        cockBehaviour.onCockAnimationEnd += OnCockingAnimationEnd;
     }
 
     public void Special(IShootable target)
