61f87e82384e8f9a227101a6e3acd540a2b647cb
Raytroll <80167098+Raytroll@users.noreply.github.com>
2026-03-13 09:20:21 +0100

    Added recording camera to the prefab + added the glowing red led

--- Assets/__Game/_Scripts/RecordingCamera.cs
+++ Assets/__Game/_Scripts/RecordingCamera.cs
@@ -1,18 +1,19 @@
 using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
+using System.Numerics;
 
 public class RecordingCamera : MonoBehaviour {
     [SerializeField] private float ViewRange = 45.0f;
     [SerializeField] private float RotateSpeed = 0.5f;
-    private float YOffset = 0.0f;
+    private UnityEngine.Vector3 RotationOffset = new UnityEngine.Vector3(0.0f, 0.0f, 0.0f);
     private float CurrentTime = 0.0f;
 
     void Start() {
-        YOffset = transform.rotation.eulerAngles.y;
+        RotationOffset = new UnityEngine.Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
     }
     void Update() {
         CurrentTime += Time.deltaTime;
-        transform.rotation = Quaternion.Euler(0f, (Mathf.Cos(CurrentTime * RotateSpeed) * ViewRange) + YOffset, 0f);
+        transform.rotation = UnityEngine.Quaternion.Euler(RotationOffset.x, (Mathf.Cos(CurrentTime * RotateSpeed) * ViewRange) + RotationOffset.y, RotationOffset.z);
     }
 }
