9df7c475e3c574eb635b9022cc4e8dba34c06cf2
MagnierHugo <magnierhugo@proton.me>
2026-03-13 12:02:47 +0100

    lot

--- Assets/__Game/Guns/Revolver/Revolver.cs
+++ Assets/__Game/Guns/Revolver/Revolver.cs
@@ -1,26 +1,57 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Revolver : Gun, IHaveSpecial
 {
-    private Mag magManager;
+    private Mag mag;
+
+    [Header("Components")]
+    [SerializeField] private ParticleSystem muzzleFlash;
+    [SerializeField] private Animator animator;
+
+    [Header("Audio")]
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip revolverCockingAudio;
+    [SerializeField] private AudioClip revolverFireAudio;
+    [SerializeField] private AudioClip revolverEmptyAudio;
 
     private void Start()
     {
-        magManager.InitMag(6);
+        mag.InitMag(6);
     }
 
     public override void Shoot(IShootable target)
     {
-        if(magManager.GetBullet())
+        animator.SetTrigger("Cock Hammer");
+        audioSource.clip = revolverCockingAudio;
+        audioSource.Play();
+        var cockBehaviour = animator.GetBehaviour<Cock_Behaviour>();
+
+        // Would have used lambda but it can't ref itself
+        void OnCockingAnimationEnd()
         {
+            cockBehaviour.onCockAnimationEnd -= OnCockingAnimationEnd;
+            bool bullet = mag.GetBullet();
+            if (!bullet)
+            {
+                audioSource.clip = revolverEmptyAudio;
+                audioSource.Play();
+            }
+
             target.GetShot();
+            muzzleFlash.Play();
+            audioSource.clip = revolverFireAudio;
+            audioSource.Play();
+
         }
+
+        cockBehaviour.onCockAnimationEnd += OnCockingAnimationEnd;
     }
 
     public void Special(IShootable target)
     {
-        magManager.AddBullet();
-        magManager.Shuffle();
+        mag.AddBullet();
+        mag.Shuffle();
         Shoot(target);
     }
 }
