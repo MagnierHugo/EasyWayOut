6ebcd2273eb93023a30becd1906282330a9e1a15
Raytroll <80167098+Raytroll@users.noreply.github.com>
2026-03-13 07:59:10 +0100

    Fixed recording camera offset rotation

--- Assets/__Game/_Scripts/RecordingCamera.cs
+++ Assets/__Game/_Scripts/RecordingCamera.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class RecordingCamera : MonoBehaviour {
     [SerializeField] private float ViewRange = 45.0f;
     [SerializeField] private float RotateSpeed = 0.5f;
-    [SerializeField] private float YOffset = 0.0f;
+    private float YOffset = 0.0f;
     private float CurrentTime = 0.0f;
 
+    void Start() {
+        YOffset = transform.rotation.eulerAngles.y;
+    }
     void Update() {
         CurrentTime += Time.deltaTime;
         transform.rotation = Quaternion.Euler(0f, (Mathf.Cos(CurrentTime * RotateSpeed) * ViewRange) + YOffset, 0f);
